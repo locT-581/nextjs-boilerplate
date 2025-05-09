a5a1719472c11c57092164382e7c4a6c54cefa56
locT-581 <loct.se581@gmail.com>
2025-05-09 11:23:15 +0700

    chore: add css baseline

--- src/theme/ThemeProvider.tsx
+++ src/theme/ThemeProvider.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import { CssBaseline } from '@mui/material';
 import { AppRouterCacheProvider } from '@mui/material-nextjs/v13-appRouter';
 import { ThemeProvider as ThemeProviderMUI } from '@mui/material/styles';
 import { theme } from '.';
@@ -11,7 +12,10 @@ export type IThemeProviderProps = {
 export default function ThemeProvider({ children }: IThemeProviderProps) {
   return (
     <AppRouterCacheProvider options={{ enableCssLayer: true }}>
-      <ThemeProviderMUI theme={theme}>{children}</ThemeProviderMUI>
+      <ThemeProviderMUI theme={theme}>
+        {children}
+        <CssBaseline />
+      </ThemeProviderMUI>
     </AppRouterCacheProvider>
   );
 }
